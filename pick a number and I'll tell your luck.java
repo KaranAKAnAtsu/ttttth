f999d4a78363938f48c9da717b916d81bc880402
KaranAKAnAtsu <92134348+KaranAKAnAtsu@users.noreply.github.com>
2021-12-24 14:30:42 +0530

    Add files via upload

---
+++ pick a number and I'll tell your luck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("pick a number between 1 and 10");
+		Scanner input = new Scanner(System.in);
+		int inputtedNum = input.nextInt();
+		if (inputtedNum <= 5) {
+			
+			System.out.println(" you are a lucky madarchod");
+		} else {
+		
+			System.out.println(" zinda kyun hai bhosdike");	
+			
+			}
+	
+		
+	}
+}
